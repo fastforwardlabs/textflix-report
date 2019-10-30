870a6dfe8c9e57e4d9b246e0a71972c128362007
Grant Custer <grantcuster@gmail.com>
2019-10-30 11:08:36 -0400

    add google analytics

--- build_html.js
+++ build_html.js
@@ -550,6 +550,17 @@ function makeHead() {
     
     ${makeStyle()}
     ${makeJS()}
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140718127-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-140718127-1');
+    </script>
   </head>`;
 }
 
